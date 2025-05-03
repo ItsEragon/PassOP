93952cffb2f5943fcad1a215d43a382a3dddfc8d
Amritpal Singh <amritps2001@gmail.com>
2025-05-03 21:33:01 +0530

    added some changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,15 +2,21 @@ import React from 'react'
 
 const Navbar = () => {
     return (
-        <nav className='bg-purple-200 flex justify-between items-center px-4 h-14'>
-            <div className="logo font-bold">Passop</div>
-            <ul>
-                <li className='flex gap-4'>
-                    <a className='hover:font-bold cursor-pointer' href="/">Home</a>
-                    <a className='hover:font-bold cursor-pointer' href="#">About</a>
-                    <a className='hover:font-bold cursor-pointer' href="#">Contact</a>
-                </li>
-            </ul>
+        <nav className='bg-slate-800 text-white'>
+            <div className="mycontainer flex justify-between items-center px-4 py-5 h-14">
+                <div className="logo font-bold text-white text-2xl">
+                    <span className='text-green-500'>&lt;</span>
+                    <span>Pass</span><span className='text-green-500'>OP/&gt;
+                    </span>
+                </div>
+                <ul>
+                    <li className='flex gap-4'>
+                        <a className='hover:font-bold cursor-pointer' href="/">Home</a>
+                        <a className='hover:font-bold cursor-pointer' href="#">About</a>
+                        <a className='hover:font-bold cursor-pointer' href="#">Contact</a>
+                    </li>
+                </ul>
+            </div>
         </nav>
     )
 }
