95e4572933fb70f68bc24fefc7ee0fe233d815b0
Amritpal Singh <amritps2001@gmail.com>
2025-05-03 15:46:58 +0530

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Navbar = () => {
+    return (
+        <nav className='bg-purple-200 flex justify-between items-center px-4 h-14'>
+            <div className="logo font-bold">Passop</div>
+            <ul>
+                <li className='flex gap-4'>
+                    <a className='hover:font-bold cursor-pointer' href="/">Home</a>
+                    <a className='hover:font-bold cursor-pointer' href="#">About</a>
+                    <a className='hover:font-bold cursor-pointer' href="#">Contact</a>
+                </li>
+            </ul>
+        </nav>
+    )
+}
+
+export default Navbar
